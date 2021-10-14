31f8fb51d2629d6bb26e98dbfca7d58b9901196d
Anllel2001 <angelesquinaspuig@gmail.com>
2021-10-14 22:56:39 +0200

    Comienzo del ejercicio sobre punteros con estructuras funcionales

---
+++ Ejercicio12/Punteros.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+string letras[80];
+
+class punteros
+{
+private:
+    /* data */
+public:
+    void puntero();
+};
+
+
+void punteros::puntero()
+{
+}
+
+
+int main(int argc, char const *argv[])
+{
+    /* code */
+    return 0;
+}
