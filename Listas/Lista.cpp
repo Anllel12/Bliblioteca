ebc38687bf518d3f1cc01c3183688ef0503c7319
Anllel2001 <angelesquinaspuig@gmail.com>
2021-11-24 19:23:41 +0100

    Comienzo Pilas

--- Listas/Lista.cpp
+++ Listas/Lista.cpp
@@ -1,1 +1,61 @@
+/*
+Vamos a desatascar el supermercado
+
+    2 clases, Caja y Cliente
+        Caja = Número de Caja, lista de clientes
+        Cliente= nombre
+    Cuando se abre una caja nueva se transfieren (en orden) los clientes a partir de cierto rango
+        Implementaremos dicha transferencia entre cajas 
+            (No hay control sobre el número máximo de clientes para la transferencia, no es necesario automatizarlo de momento)
+        Si no están todos en fila, es normal preguntar quién es el último (lo implementamos por caja)
+        La persona encargada de despacharnos en algún caso podrá preguntar quién es el siguiente (lo implementamos por caja)
+            Asumimos que el primero/a de la lista ha sido atendido/a
+
+
+Angel Esquinas Puig
+*/
+
 #include <iostream>
+
+using namespace std;
+
+class caja
+{
+    private:
+        
+    public:
+        caja();
+        ~caja();
+};
+
+caja::caja()
+{
+}
+
+caja::~caja()
+{
+}
+
+class cliente
+{
+    private:
+        
+    public:
+        cliente();
+        ~cliente();
+};
+
+cliente::cliente()
+{
+}
+
+cliente::~cliente()
+{
+}
+
+
+int main(int argc, char const *argv[])
+{
+    
+    return 0;
+}
---
+++ Pilas/Pila.cpp
@@ -0,0 +1,22 @@
+/*
+Como hemos podido observar en los apuntes, las pilas tienen un orden implícito directamente 
+relacionado con la estructura de la misma, ergo, podría utilizarse para “ordenar” en nuestros programas.
+
+    Crear una estructura (array o vector) que contenga una serie de números insertados con un valor aleatorio en los distintos nodos de la estructura.
+    Utilizar una pila como pivote para ordenar de mayor a menor ( o a la inversa)
+        La pila tiene que estar vacía al principio de la ejecución y al final.
+
+Angel Esquinas Puig
+*/
+
+#include <iostream>
+
+using namespace std;
+
+
+
+int main(int argc, char const *argv[])
+{
+    
+    return 0;
+}
